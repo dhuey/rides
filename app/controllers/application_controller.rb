3e0471770db33c7784f872f36255408fa884ae97
Dalton Huey <daltonjhuey@gmail.com>
2019-11-14 15:19:20 -0600

    Configure logins and user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :verify_logged_in
+
+  def verify_logged_in
+    if user_signed_in?
+      return true
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
