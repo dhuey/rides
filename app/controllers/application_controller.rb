2f09ef332476c3730f22c44ef2ff13983cb406d9
Dalton Huey <daltonjhuey@gmail.com>
2019-12-01 18:29:27 -0600

    Add custom user fields when signing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :verify_logged_in
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def verify_logged_in
-    if user_signed_in?
-      return true
-    else
-      redirect_to new_user_session_path
-    end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender,  :phone_number, :nationality, :ministry])
   end
 end
