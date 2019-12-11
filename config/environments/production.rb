5ec5dba96be24adf22ec9fefc277e80fd32777c9
Dalton Huey <daltonjhuey@gmail.com>
2019-12-10 22:24:40 -0600

    Back to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'secureus187.sgcpanel.com',
-    port:                 465,
+    address:              'smtp.gmail.com',
+    port:                 587,
     domain:               'link-rides.herokuapp.com',
     user_name:            ENV['MAILER_EMAIL'],
     password:             ENV['MAILER_PASSWORD'],
