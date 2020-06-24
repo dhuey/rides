5f0b6e900a0877cf6860b1729f4865087e17113e
Dalton Huey <daltonjhuey@gmail.com>
2020-06-24 16:33:47 -0500

    Finish driver styling

--- app/helpers/rides_helper.rb
+++ app/helpers/rides_helper.rb
@@ -1,2 +1,27 @@
 module RidesHelper
+  def ride_message(ride)
+    if current_user.international?
+      if ride.completed?
+        "You got a ride from #{ride.driver.full_name} on #{ride.formatted_pickup_time}."
+      elsif ride.claimed?
+        "You have a ride scheduled for #{ride.formatted_pickup_time} with #{ride.driver.full_name}."
+      else
+        "Your ride for #{ride.formatted_pickup_time} is still awaiting a driver."
+      end
+    else
+      if ride.completed?
+        "You gave #{ride.requester.full_name} a ride on #{ride.formatted_pickup_time}."
+      else
+        "You are scheduled to give #{ride.requester.full_name} a ride on #{ride.formatted_pickup_time}."
+      end
+    end
+  end
+
+  def driver_back_button
+    if URI(request.referrer).path == "/pending_rides"
+      link_to "< Rides awaiting a driver", pending_rides_path
+    else
+      link_to "< Your rides", rides_path
+    end
+  end
 end
