1396095fc1ad0dc2f7865127b0e54139a8ca859b
Dalton Huey <daltonjhuey@gmail.com>
2020-06-26 15:18:15 -0500

    Fixes #13 -> add bots and groupmes for male/female passengers

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -13,20 +13,38 @@ class Notifier
   end
 
   def request_ride
-    uri = URI.parse("https://api.groupme.com/v3/bots/post")
-    request = Net::HTTP::Post.new(uri)
-    request.content_type = "application/json"
-    request.body = JSON.dump({
-      "bot_id" => "87bd98db787a285619bbb9490a",
-      "text" => "A new ride for #{@number_of_passengers} passenger(s) has been requested for #{@pickup_time}. #{Rails.application.routes.url_helpers.ride_url(@ride)}"
-    })
+    if @requester_gender == "male"
+      uri = URI.parse("https://api.groupme.com/v3/bots/post")
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = "application/json"
+      request.body = JSON.dump({
+        "bot_id" => "87bd98db787a285619bbb9490a",
+        "text" => "A new ride for #{@number_of_passengers} passenger(s) has been requested for #{@pickup_time}. #{Rails.application.routes.url_helpers.ride_url(@ride)}"
+      })
 
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
 
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+    else
+      uri = URI.parse("https://api.groupme.com/v3/bots/post")
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = "application/json"
+      request.body = JSON.dump({
+        "bot_id" => "a7c4ba60bb757c4949a15d20d5",
+        "text" => "A new ride for #{@number_of_passengers} passenger(s) has been requested for #{@pickup_time}. #{Rails.application.routes.url_helpers.ride_url(@ride)}"
+      })
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
     end
   end
 end
