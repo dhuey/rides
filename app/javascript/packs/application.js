50c1b30a97df33e7f699edce63961c89a91facd2
Dalton Huey <daltonjhuey@gmail.com>
2020-06-26 13:51:07 -0500

    styled international's view, fixed datetimepicker (#4)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,17 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import flatpickr from "flatpickr"
+require ("flatpickr/dist/flatpickr.css")
+
+document.addEventListener("turbolinks:load", () => {
+  flatpickr(".timepicker", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    minDate: "today"
+  })
+});
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
