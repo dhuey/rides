b2442a4bb7b4e63cc254c91ec2548da0d4c973ea
dhuey <daltonjhuey@gmail.com>
2020-06-26 19:07:20 -0500

    Add user profile pages (#20)
    * Add user controller
    
    * Move narrow page css to body element, not a class. This way every page has it by default
    
    * add user show page
    
    * Add editing/updating actions and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "We updated your profile!"
+    else
+      render "edit", alert: "We couldn't update your profile. Let's try again."
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :alternate_contact_method, :alternate_contact_name, :phone, :ministry, :email_interest)
+  end
+end
