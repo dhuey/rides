df88899302ccfab62760d2342cfa4e5a4062286e
Dalton Huey <daltonjhuey@gmail.com>
2020-07-02 16:56:42 -0500

    finish vehicles migration

--- db/migrate/20200629200648_create_vehicles.rb
+++ db/migrate/20200629200648_create_vehicles.rb
@@ -1,7 +1,12 @@
 class CreateVehicles < ActiveRecord::Migration[6.0]
   def change
     create_table :vehicles do |t|
-
+      t.belongs_to :user, null: false
+      t.string :year, null: false
+      t.string :color, null: false
+      t.string :make, null: false
+      t.string :model, null: false
+      t.string :license_plate, null: false
       t.timestamps
     end
   end
