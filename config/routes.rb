1d7d89420c5887be3e395a5f53bfdf46b520b61e
Dalton Huey <daltonjhuey@gmail.com>
2020-07-13 21:38:08 -0500

    only add letter_opener_web route on staging

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  if Rails.env.staging?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   resources :rides
