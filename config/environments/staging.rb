7c0a45e20a472603e1294ebbee1d10c8cdf08476
Dalton Huey <daltonjhuey@gmail.com>
2020-07-13 21:10:48 -0500

    change delivery method to letter_opener_web on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rides_production"
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = {
