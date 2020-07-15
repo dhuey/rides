92b24848e4a7c03c91f4ba96ff9343e6598b68ba
Dalton Huey <daltonjhuey@gmail.com>
2020-07-14 21:47:14 -0500

    fix positioning

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,12 +22,12 @@ if (document.readyState === "interactive") {
 document.addEventListener("turbolinks:load", () => {
   flatpickr(".timepicker", {
     enableTime: true,
-    dateFormat: "Y-m-d H:i",
+    dateFormat: "M d, Y at h:i K",
     minDate: "today",
-    static: true
+    minTime: Date.now(),
+    appendTo: window.document.querySelector('#time-wrap')
   })
 });
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
