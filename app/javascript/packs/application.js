f9ee3f417008995024021f5381e957b11b556c95
dhuey <daltonjhuey@gmail.com>
2020-07-14 22:10:02 -0500

    Fix time picker on computers (#25)
    * Use static: true for flatpickr and disable autocomplete on text field so it doesn't get in the way
    
    * fix positioning

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,11 +22,12 @@ if (document.readyState === "interactive") {
 document.addEventListener("turbolinks:load", () => {
   flatpickr(".timepicker", {
     enableTime: true,
-    dateFormat: "Y-m-d H:i",
-    minDate: "today"
+    dateFormat: "M d, Y at h:i K",
+    minDate: "today",
+    minTime: Date.now(),
+    appendTo: window.document.querySelector('#time-wrap')
   })
 });
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
