20f5f02d1ce331a183a5da648043c267a545ebae
Dalton Huey <daltonjhuey@gmail.com>
2020-07-16 15:17:27 -0500

    start better styling of radio buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,13 @@ document.addEventListener("turbolinks:load", () => {
   })
 });
 
+ $('.type-select').click(function () {
+     $('.type-select').removeClass('type-selected');
+     $(this).addClass('type-selected');
+ });
+
+$('.type-radio:checked').parent().addClass('type-selected');
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
