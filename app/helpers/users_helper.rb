0b6a243f15dbf2147a8549a8359b8e6a1629a890
Dalton Huey <daltonjhuey@gmail.com>
2020-08-18 15:58:49 -0500

    Make gender, alt_contact_method editable by admins

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,26 @@
 module UsersHelper
+  def opposite_gender(user)
+    if user.gender == "male"
+      "female"
+    else
+      "male"
+    end
+  end
+
+  def contact_methods(user)
+    case user.alternate_contact_method
+    when "WeChat"
+      [["WeChat", "WeChat"], ["Facebook", "Facebook"], ["Whatsapp", "Whatsapp"], ["Instagram", "Instagram"], ["Line", "Line"], ["None", "nil"]]
+    when "Facebook"
+      [["Facebook", "Facebook"], ["WeChat", "WeChat"], ["Whatsapp", "Whatsapp"], ["Instagram", "Instagram"], ["Line", "Line"], ["None", "nil"]]
+    when "Whatsapp"
+      [["Whatsapp", "Whatsapp"], ["WeChat", "WeChat"], ["Facebook", "Facebook"], ["Instagram", "Instagram"], ["Line", "Line"], ["None", "nil"]]
+    when "Instagram"
+      [["Instagram", "Instagram"], ["WeChat", "WeChat"], ["Facebook", "Facebook"], ["Whatsapp", "Whatsapp"], ["Line", "Line"], ["None", "nil"]]
+    when "Line"
+      [["Line", "Line"], ["WeChat", "WeChat"], ["Facebook", "Facebook"], ["Whatsapp", "Whatsapp"], ["Instagram", "Instagram"], ["None", "nil"]]
+    else
+      [["Please select one", "nil"], ["WeChat", "WeChat"], ["Facebook", "Facebook"], ["Whatsapp", "Whatsapp"], ["Instagram", "Instagram"], ["Line", "Line"], ["None", "nil"]]
+    end
+  end
 end
