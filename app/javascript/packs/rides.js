8c019e6f59394c91d93393a89cddb1be7b7bddd0
Dalton Huey <daltonjhuey@gmail.com>
2020-08-17 19:45:37 -0500

    Should load vehicle button on initial load and refresh

--- app/javascript/packs/rides.js
+++ app/javascript/packs/rides.js
@@ -1,11 +1,11 @@
-$("#which-vehicle").click(function() {
+$("#which-vehicle").on('click', function() {
   $(this).hide();
   $("#vehicle-select").animate({
     height: 'show'
   });
 });
 
-$("#no-vehicle").click(function() {
+$("#no-vehicle").on('click', function() {
   $(this).hide();
   $("#vehicle-add").animate({
     height: 'show'
