d9f02abd911917f5e126d9c80973c4fda8265ae6
Dalton Huey <daltonjhuey@gmail.com>
2022-04-19 15:37:49 -0500

    style signup form and fix radio javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,18 +32,23 @@ document.addEventListener("turbolinks:load", () => {
     defaultMinute: rounded.getMinutes(),
     appendTo: window.document.querySelector('#time-wrap')
   })
+
+  let checkedRadio = document.querySelector('.type-radio:checked');
+  if (checkedRadio) {
+    checkedRadio.parentElement.classList.add('type-selected');
+  };
+
+  document.addEventListener("turbolinks:render", () => {
+    if (checkedRadio) {
+      checkedRadio.parentElement.classList.add('type-selected');
+    }
+  })
+
+  document.addEventListener('click', e => {
+    let radioAncestor = e.target.closest('.type-select');
+    if (radioAncestor && !radioAncestor.classList.contains('type-selected')) {
+      document.querySelector('.type-selected').classList.remove('type-selected');
+      radioAncestor.classList.add('type-selected');
+    }
+  })
 });
-
- $('.type-select').click(function () {
-     $('.type-select').removeClass('type-selected');
-     $(this).addClass('type-selected');
- });
-
-$('.type-radio:checked').parent().addClass('type-selected');
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
