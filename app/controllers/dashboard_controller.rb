e3cb6d4f8da2ff2478e144de5d0da43770a29b19
Dalton Huey <daltonjhuey@gmail.com>
2022-04-20 10:12:04 -0500

    redesigned dashboard for drivers, internationals, and admin

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    if current_user.international?
+      @rides = Ride.unarchived.where(requester_id: current_user.id).order(pickup_time: :desc)
+    else
+      @rides = Ride.unarchived.where(driver_id: current_user.id).order(pickup_time: :desc)
+    end
   end
 end
