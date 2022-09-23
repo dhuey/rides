a2a177bff3f1dbdb0b162f4733c58f47d54c734c
Dalton Huey <daltonjhuey@gmail.com>
2022-09-23 17:36:52 -0500

    fix rides show page js and links

--- app/javascript/rides.js
+++ app/javascript/rides.js
@@ -1,15 +1,33 @@
 import $ from "jquery";
 
-$("#which-vehicle").on("click", function () {
-  $(this).hide();
-  $("#vehicle-select").animate({
-    height: "show",
+document.addEventListener("DOMContentLoaded", () => {
+  $("#which-vehicle").on("click", function () {
+    $(this).hide();
+    $("#vehicle-select").animate({
+      height: "show",
+    });
+  });
+
+  $("#no-vehicle").on("click", function () {
+    $(this).hide();
+    $("#vehicle-add").animate({
+      height: "show",
+    });
   });
 });
 
-$("#no-vehicle").on("click", function () {
-  $(this).hide();
-  $("#vehicle-add").animate({
-    height: "show",
+document.addEventListener("turbo:render", () => {
+  $("#which-vehicle").on("click", function () {
+    $(this).hide();
+    $("#vehicle-select").animate({
+      height: "show",
+    });
+  });
+
+  $("#no-vehicle").on("click", function () {
+    $(this).hide();
+    $("#vehicle-add").animate({
+      height: "show",
+    });
   });
 });
